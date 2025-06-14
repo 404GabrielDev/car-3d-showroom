66f102ea0e15d40fb9da01e924c3bdd77ee29e17
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-06-14 11:56:18 -0300

    feat: Logo version for desktop

---
+++ src/components/logoAnimation/Logo.tsx
@@ -0,0 +1,16 @@
+import "./Logo.scss";
+
+const Logo = () => {
+  return (
+    <h1 className="home-title" data-aos="fade-zoom-in" data-aos-duration="1500" data-aos-delay="500">
+      <p >
+        Performance in every line
+      </p>
+      <span>
+        Power in every detail
+      </span>
+    </h1>
+  );
+};
+
+export default Logo;
