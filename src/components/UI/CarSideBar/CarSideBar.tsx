cee3921f11ac50b212d209ea2efbf0db0264c28e
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-06-20 13:43:10 -0300

    feat: create home components

---
+++ src/components/UI/CarSideBar/CarSideBar.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useRef, useState } from "react";
+import "./CarSideBar.css";
+import ToggleButton from "../ToggleButton/ToggleButton";
+import { useLoading } from "../../Context/LoadingContext/LoadingContext";
+import LoadingCar from "../LoadingCar/LoadingCar";
+import { AnimatePresence, motion } from "framer-motion";
+import Loading from "../LoadingCar/LoadingCar";
+//OTIMIZAR BOTÃO
+function SideBar({
+  currentCarIndex,
+  handleSelectCar: originalHandleSelectCar,
+  cars,
+}) {
+  const [sideBar, setSideBar] = useState(false);
+
+  const showSideBar = () => setSideBar(!sideBar);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const time = (index) => {
+    if (currentCarIndex === index) {
+      setIsLoading(false);
+      return;
+    }
+    setIsLoading(true);
+    setSideBar(false);
+    const timeLoading = setTimeout(() => {
+      setIsLoading(false);
+    }, 2800);
+  };
+
+  const timeHandleIndex = (index) => {
+    const timeHandle = setTimeout(() => {
+      originalHandleSelectCar(index);
+    }, 1000);
+  };
+
+  return (
+    <>
+      <AnimatePresence>
+        {isLoading && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, transition: { duration: 0.5 } }}
+            transition={{ duration: 1 }}
+            className="loading-container"
+          >
+            <Loading />
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+        <div className={`container-gallery ${sideBar ? 'open' : ''}`}>
+          <div className="container-toggleBtn" onClick={showSideBar}>
+            <ToggleButton />
+          </div>
+
+          <div className="icons-showroom">
+          {cars.map((car, index) => (
+            <img
+              key={car.name}
+              src={car.thumb}
+              alt={car.name}
+              onClick={() => {
+                timeHandleIndex(index);
+                time(index);
+              }}
+              className={index === currentCarIndex ? "selected" : ""}
+              style={{ cursor: "pointer" }}
+            />
+          ))}
+          </div>
+        </div>
+    </>
+  );
+}
+
+export default SideBar;
