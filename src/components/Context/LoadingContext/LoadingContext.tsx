da51531f332d1b620c9e65168ea14e5f3a9dd6f9
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-06-20 13:39:18 -0300

    feat: create components for loading

---
+++ src/components/Context/LoadingContext/LoadingContext.tsx
@@ -0,0 +1,16 @@
+// LoadingContext.tsx
+import React, { createContext, useContext, useState } from "react";
+
+const LoadingContext = createContext(null);
+
+export const LoadingProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
+
+export const useLoading = () => useContext(LoadingContext);
