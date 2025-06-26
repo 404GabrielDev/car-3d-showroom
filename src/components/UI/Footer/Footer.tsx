1472df4b7dba41c9251e1f36c77cc653d28da7f4
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-06-26 10:55:44 -0300

    feat(footer): add new footer component

---
+++ src/components/UI/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+
+const Footer = () => {
+    return (
+        <>
+        <p id='pFooter'>
+          © 2025 | CarbonDream — Developed by <span>João Gabriel</span>
+        </p>
+        </>
+    )
+}
+
+export default Footer;
